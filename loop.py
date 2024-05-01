bd2391c2c9c01600c5271d3853e0f59f6265eac6
Julian Sedillo <jsedillo@amd.com>
2024-05-01 10:38:16 -0400

    changed directory search to be based on file name instead of file position

--- loop.py
+++ loop.py
@@ -7,6 +7,25 @@ OUTPUT_DIR = "./output"
 SKIP_INTERVAL = 2
 
 
+def extract_frame_index(path):
+  index = None
+
+  for fileNameSegment in os.path.basename(path).split("_"):
+    try:
+      index = int(fileNameSegment)
+    except Exception:
+      continue
+
+  return index
+
+
+def generate_flowdepth_path(index, input=SOURCE_DIR):
+  vel_file = f"{input}/vel/{index:06d}_vel.exr"
+  dep_file = f"{input}/dep/{index:06d}_dep.tif"
+
+  return vel_file, dep_file
+
+
 def loop(input=SOURCE_DIR, output=OUTPUT_DIR, interval=SKIP_INTERVAL, vel=False, dep=False):
   # Create subdirectory based on config
   out_dir = output + "/output_using"
@@ -21,31 +40,30 @@ def loop(input=SOURCE_DIR, output=OUTPUT_DIR, interval=SKIP_INTERVAL, vel=False,
   # Load list of files
   img_dir = input + "/col"
   src_imgs = os.listdir(img_dir)
-  if vel:
-    vel_dir = input + "/vel"
-    src_vels = os.listdir(vel_dir)
-  if dep:
-    dep_dir = input + "/dep"
-    src_deps = os.listdir(dep_dir)
 
   for i in range(0, len(src_imgs) - interval, interval):
     img_1 = os.path.join(img_dir, src_imgs[i])
     img_2 = os.path.join(img_dir, src_imgs[i + interval])
 
     cmd = f"{BASE_CMD} --one {img_1} --two {img_2}"
+    
+    idx_1 = extract_frame_index(img_1)
+    idx_2 = extract_frame_index(img_2)
+    vel_1, dep_1 = generate_flowdepth_path(idx_1, input=input)
+    vel_2, dep_2 = generate_flowdepth_path(idx_2, input=input)
 
     if vel:
-      vel_1 = os.path.join(vel_dir, src_vels[int(i / interval)])
-      vel_2 = os.path.join(vel_dir, src_vels[int(i / interval) + 1])
+      assert os.path.exists(vel_1), f"File {vel_1} could not be found."
+      assert os.path.exists(vel_2), f"File {vel_2} could not be found."
       cmd += f" --floOne {vel_1} --floTwo {vel_2}"
 
     if dep:
-      dep_1 = os.path.join(dep_dir, src_deps[int(i / interval)])
-      dep_2 = os.path.join(dep_dir, src_deps[int(i / interval) + 1])
+      assert os.path.exists(dep_1), f"File {dep_1} could not be found."
+      assert os.path.exists(dep_2), f"File {dep_2} could not be found."
       cmd += f" --depOne {dep_1} --depTwo {dep_2}"
 
     # Generate output file name
-    i_interp = (2 * i + interval) / 2
+    i_interp = (idx_1 + idx_2) / 2
     file_out = f"{i_interp:08.1f}" if i_interp % 1 else f"{int(i_interp):06d}"
     file_out = f"{out_dir}/{file_out}_interp.png"
     cmd += f" --out {file_out}"
